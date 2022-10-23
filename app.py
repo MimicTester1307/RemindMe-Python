e2534e683c05398483e7584a9540ae2082ccbe8d
MimicTester1307 <excel.chukwu@ashesi.edu.gh>
2022-10-23 14:04:41 +0000

    create file for handling forms
    Signed-off-by: MimicTester1307 <excel.chukwu@ashesi.edu.gh>

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
 
 @app.route('/')
 def show_home():
     return render_template('index.html')
+
+
+@app.route('/login')
+def show_login():
+    return render_template('login.html')  # TODO: Fix login routing from index page
 
 
 if __name__ == '__main__':
